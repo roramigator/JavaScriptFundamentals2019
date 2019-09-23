8a3fe0743f4c4662aee5f36b98da0656aaccee99
roramigator <roramigator@gmail.com>
2019-09-23 19:15:37 -0400

    Update challenges

--- exercises/03-intro-challenges/intro-challenges.js
+++ exercises/03-intro-challenges/intro-challenges.js
@@ -8,7 +8,27 @@
  * countNumberOfDigits(123); // 3
  * countNumberOfDigits(1000); // 4
  */
-function countNumberOfDigits(num) {}
+function countNumberOfDigits(num) {
+  /**
+   * str = num+'';
+   * return str.length;
+   */
+
+  let count = 0;
+  if (num > -10 && num < 10) {
+    count = 1;
+  }
+  if (num > 9 && num < 100) {
+    count = 2;
+  }
+  if (num > 99 && num < 1000) {
+    count = 3;
+  }
+  if (num > 999 && num < 10000) {
+    count = 4;
+  }
+  return count;
+}
 
 /**
  * Given a number of seconds, return how long ago something happened.
@@ -24,7 +44,22 @@ function countNumberOfDigits(num) {}
  * getRelativeTime(59); // seconds ago
  * getRelativeTime(60); // minutes ago
  */
-function getRelativeTime(seconds) {}
+function getRelativeTime(seconds) {
+  let happened = "";
+  if (seconds > 0 && seconds < 60) {
+    happened = "seconds ago";
+  }
+  if (seconds > 59 && seconds < 3600) {
+    happened = "minutes ago";
+  }
+  if (seconds > 3599 && seconds < 86400) {
+    happened = "hours ago";
+  }
+  if (seconds > 86399) {
+    happened = "days ago";
+  }
+  return happened;
+}
 
 /**
  * Round to the nearest 100th decimal point.
@@ -35,7 +70,10 @@ function getRelativeTime(seconds) {}
  * roundToNearestHundredth(14.511); // 14.51
  * roundToNearestHundredth(14.499); // 14.5
  */
-function roundToNearestHundredth(num) {}
+function roundToNearestHundredth(num) {
+  let round = Math.round(num * 100) / 100;
+  return round;
+}
 
 // Ignore this. It is for the tests.
 module.exports = {
